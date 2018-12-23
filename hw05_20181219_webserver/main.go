9fb85d263a8764771c271b65877db9770c448dc3
alex.kondratyuk <sankoniks@gmail.com>
2018-12-23 18:53:41 +0200

    added struct for keeping user's info

--- hw05_20181219_webserver/main.go
+++ hw05_20181219_webserver/main.go
@@ -7,48 +7,56 @@ import (
 	"strings"  // пакет для работы с  UTF-8 строками
 )
 
+var name, lastname, age string = "New", "User", "16"
+
+type User struct {
+	Name     string
+	Lastname string
+	Age      string
+}
+
 func main() {
-	http.HandleFunc("/", HomeRouterHandler) // установим роутер
-	http.HandleFunc("/new", NewRouterHandler)
+	userMain := &User{"? Guest Name", "? Guest LastNane", "? Guest Age"}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		fmt.Fprintf(w, "Hello %s %s", name, lastname)
+	})
+
+	http.HandleFunc("/new", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm() //анализ аргументов,
+
+		for k, v := range r.Form {
+			switch k {
+			case "name":
+				userMain.setName(strings.Join(v, ""))
+			case "lastname":
+				userMain.setLastName(strings.Join(v, ""))
+			case "age":
+				userMain.setAge(strings.Join(v, ""))
+			}
+		}
+
+		fmt.Fprintf(w, "Your name is %s! \n", userMain.Name)
+		fmt.Fprintf(w, "Your surname is  %s \n", userMain.Lastname)
+		fmt.Fprintf(w, "You are %s years old \n", userMain.Age)
+	})
+
 	err := http.ListenAndServe(":3000", nil) // задаем слушать порт
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+	fmt.Println(userMain)
 }
 
-var name, lastname, age string = "New", "User", "16"
-
-func HomeRouterHandler(w http.ResponseWriter, r *http.Request) { // дефолтное view
-	r.ParseForm() //анализ аргументов,
-
-	for k, v := range r.Form {
-
-		switch k {
-		case "name":
-			name = strings.Join(v, "")
-		case "lastname":
-			lastname = strings.Join(v, "")
-		}
-	}
-	fmt.Fprintf(w, "Hello %s %s", name, lastname) // отправляем данные на клиентскую сторону
+func (user *User) setName(name string) {
+	user.Name = name
 }
 
-func NewRouterHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm() //анализ аргументов,
+func (user *User) setLastName(lastname string) {
+	user.Lastname = lastname
+}
 
-	for k, v := range r.Form {
-		fmt.Println("key:", k, "val:", strings.Join(v, ""))
-
-		switch k {
-		case "name":
-			name = strings.Join(v, "")
-			fmt.Fprintf(w, "Your name is %s! \n", name)
-		case "lastname":
-			lastname = strings.Join(v, "")
-			fmt.Fprintf(w, "Your surname is  %s \n", lastname)
-		case "age":
-			age = strings.Join(v, "")
-			fmt.Fprintf(w, "You are %s years old \n", age)
-		}
-	}
+func (user *User) setAge(age string) {
+	user.Age = age
 }
