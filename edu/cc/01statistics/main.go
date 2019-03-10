7606d6345a09045d250a8dcd6aa6042037f5585c
alex.kondratyuk <sankoniks@gmail.com>
2019-03-10 23:32:39 +0200

    calculated avarage datas

--- edu/cc/01statistics/main.go
+++ edu/cc/01statistics/main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"encoding/csv"
-	"fmt"
 	"os"
 	"strconv"
 )
 
 func main() {
-	f, err :=os.Open("Environmental_Data_Deep_Moor_2015.txt")
+	f, err := os.Open("Environmental_Data_Deep_Moor_2015.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -23,12 +22,17 @@ func main() {
 		panic(err)
 	}
 
+	var atTotal, bmTotal, wsTotal, counter float64
+
 	for i, row := range rows {
-		if i != 0 && i < 10 {
+		if i != 0 {
 			at, _ := strconv.ParseFloat(row[1], 64)
 			bm, _ := strconv.ParseFloat(row[2], 64)
 			ws, _ := strconv.ParseFloat(row[7], 64)
-			fmt.Printf("%T %T %T \n",at, bm, ws)
+			atTotal += at
+			bmTotal += bm
+			wsTotal += ws
+			counter++
 		}
 	}
 }
